504130940a78becb75850148719350e71f459641
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-05-19 21:48:26 +0600

    crud opeations added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { MongoClient, ServerApiVersion } from "mongodb";
-const ObjectId = require( 'mongodb' ).ObjectId;
+import { ObjectId } from "mongodb";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,7 +25,26 @@ const run = async () => {
         await client.db( 'admin' ).command( { ping: 1 } );
         console.log( "Pinged your deployment. You successfully connected to MongoDB!" );
 
+        const serviceCollection = client.db( 'cardoctor' ).collection( 'services' );
+        const orderCollection = client.db( 'cardoctor' ).collection( 'orders' );
 
+        app.get( '/services', async ( req, res ) => {
+            const cursor = serviceCollection.find();
+            const services = await cursor.toArray();
+            res.send( services );
+        } );
+
+        app.get( '/service/:_id', async ( req, res ) => {
+            const { _id } = req.params;
+            const service = await serviceCollection.findOne( { _id: new ObjectId( _id ) } );
+            res.send( service );
+        } );
+
+        app.post( '/orders', async ( req, res ) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne( order );
+            res.send( result );
+        } );
     }
     finally {
         // await client.close();
